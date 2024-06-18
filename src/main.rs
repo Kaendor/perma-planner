9bc55e872c0ac78aac5ec2c63f546862d02f72e5
Kaendor <dimteboul@gmail.com>
2024-06-18 18:57:58 +0200

    add geo dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use geojson::GeoJson;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let body =
-        reqwest::blocking::get("https://apicarto.ign.fr/api/cadastre/commune?code_insee=01384")?
-            .text()?;
-    println!("body = {:?}", body);
+    let geo_json: GeoJson =
+        reqwest::blocking::get("https://apicarto.ign.fr/api/cadastre/commune?code_insee=75056")?
+            .json()?;
+    println!("body = {:?}", geo_json);
 
     Ok(())
 }
